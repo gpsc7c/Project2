449f2f2f935096b68bd7db994b39c970dddf673d
gpsc7c <gpsc7c@delmar.umsl.edu>
2024-03-26 22:52:31 -0500

    commented up

--- parser.c
+++ parser.c
@@ -8,21 +8,29 @@
 #include "parser.h"
 #include "node.h"
 #include "tree.h"
-
+///////enumeration reminder
 //enum tokenTypes{IDTK, INTTK, ASSIGNTK, GREATTK, LESSTK, ISEQUALTK, NOTEQUALTK, COLONTK, COLONEQLTK, PLUSTK, MINUSTK, MULTIPLYTK, DIVIDETK, EXPONTK, DOTTK, OPENPARENTK, CLOSEPARENTK, COMMATK, OPENCURLTK, CLOSECURLTK, SEMICOLONTK, OPENSQUARETK, CLOSESQUARETK, ORTK, ANDTK, STARTTK, STOPTK, WHILETK, REPEATTK, UNTILTK, LABELTK, RETURNTK, CINTK, COUTTK, TAPETK, JUMPTK, IFTK, THENTK, PICKTK, CREATETK, SETTK, FUNCTK, EOFTK};
 #ifndef PARSER_C
 #define PARSER_C
+//generic error handler, forces exit after output
 void error(){
 	fprintf(stderr, "\nAn error has occurred.\n");
 	exit(-1);
 }
-node* R0(Ttoken* tk, FILE* file, char* c, int* row, int* col){
-	node* nNode = newNode(nonterms[25]);
-	if(tk->ID == GREATTK){
-		nNode->one = newTermNode(tk);
-		*tk = scanner(file, c, row, col);	
-		return nNode;
-	}
+///////////////////////////////////////////////////
+//Parse tree node for R0 (comparison/etc)//////////
+///////////////////////////////////////////////////
+//FORMAT IN R FUNCS IS FOLLOWED IN ALL OTHER NODE//
+//RETURNING FUNCTIONS: USE AS LEGEND///////////////
+///////////////////////////////////////////////////
+node* R0(Ttoken* tk, FILE* file, char* c, int* row, int* col){//passing in all to keep track of location in file
+	node* nNode = newNode(nonterms[25]);//creation of node of type in func name
+	if(tk->ID == GREATTK){//block checks for token
+		nNode->one = newTermNode(tk);//if token is correct, add it as a terminal
+					     //else move on
+		*tk = scanner(file, c, row, col);//move scanner along
+		return nNode;//in case where this is the final token, move along
+	}//nodes are assigned by the order they will be in, additional node types added where branches > 4
 	if(tk->ID == LESSTK){
 		nNode->one = newTermNode(tk);
 		*tk = scanner(file, c, row, col);	
@@ -51,16 +59,18 @@ node* R0(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 			}
 		}
 	}
-	error();
+	error();//in case where an error occurs
 	return NULL;
 }
+//additional node for the <expression>/<R0>/<expression> that R0 is always a part of
 node* RTriplet(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	node* nNode = newNode(nonterms[24]);
-	nNode->one = expr(tk, file, c, row, col);
+	nNode->one = expr(tk, file, c, row, col);//send to next  function when what is needed is not a token
 	nNode->two = R0(tk, file, c, row, col);
 	nNode->three = expr(tk, file, c, row, col);
-	return nNode;	
+	return nNode;
 }
+//additional node to break down [ <RTriplet> ] as its own, as R never appears outside of this, effective end goal is to reduce R to 3s
 node* RBrackets(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	node* nNode = newNode(nonterms[23]);
 	if(tk->ID == OPENSQUARETK){
@@ -76,7 +86,7 @@ node* RBrackets(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	error();
 	return NULL;
 }
-
+//required node for goto pattern: jumptk IDtk
 node* gotochk(Ttoken* tk, FILE* file, char* c, int* row, int* col, bool* found){
 	node* nNode = newNode(nonterms[27]);
 	if(tk->ID == JUMPTK){
@@ -93,6 +103,7 @@ node* gotochk(Ttoken* tk, FILE* file, char* c, int* row, int* col, bool* found){
 	}
 	return nNode;
 }
+//required node for label pattern: labeltk IDtk
 node* label(Ttoken* tk, FILE* file, char* c, int* row, int* col, bool* found){
 	node* nNode = newNode(nonterms[26]);
 	if(tk->ID == LABELTK){
@@ -109,9 +120,10 @@ node* label(Ttoken* tk, FILE* file, char* c, int* row, int* col, bool* found){
 	}
 	return nNode;
 }
+//required node for assignment pattern: st identifier = <expr> | Idtk = <expr>
 node* assign(Ttoken* tk, FILE* file, char* c, int* row, int* col, bool* found){
 	node* nNode = newNode(nonterms[22]);
-	bool set = false;
+	bool set = false;//flag to ensure return can't be reached from missing IDTK
 	if(tk->ID == SETTK){
 		nNode->one = newTermNode(tk);
 		*tk = scanner(file, c, row, col);
@@ -134,6 +146,7 @@ node* assign(Ttoken* tk, FILE* file, char* c, int* row, int* col, bool* found){
 	}
 	return NULL;
 }
+//required node for loop2 pattern: Repeat <stat> Until RBrackets
 node* loop2(Ttoken* tk, FILE* file, char* c, int* row, int* col, bool* found){
 	node* nNode = newNode(nonterms[21]);
 	if(tk->ID == REPEATTK){
@@ -151,6 +164,7 @@ node* loop2(Ttoken* tk, FILE* file, char* c, int* row, int* col, bool* found){
 	}
 	return NULL;	
 }
+//required node for loop1 pattern: while <RBrackets> <stat>
 node* loop1(Ttoken* tk, FILE* file, char* c, int* row, int* col, bool* found){
 	node* nNode = newNode(nonterms[20]);
 	if(tk->ID == WHILETK){
@@ -163,6 +177,7 @@ node* loop1(Ttoken* tk, FILE* file, char* c, int* row, int* col, bool* found){
 	}
 	return NULL;
 }
+//required node for pickbody pattern: <stat> : <stat>
 node* pickbody(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	node* nNode = newNode(nonterms[19]);
 	nNode->one = stat(tk, file, c, row, col);
@@ -175,6 +190,7 @@ node* pickbody(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	error();
 	return NULL;
 }
+//required node for pick pattern: Repeat <stat> Until <RBrackets>
 node* pick(Ttoken* tk, FILE* file, char* c, int* row, int* col, bool* found){
 	node* nNode = newNode(nonterms[18]);
 	if(tk->ID == PICKTK){
@@ -187,6 +203,7 @@ node* pick(Ttoken* tk, FILE* file, char* c, int* row, int* col, bool* found){
 	}
 	return NULL;
 }
+//required node for if pattern: If <RBrackets> then <stat>
 node* ifchk(Ttoken* tk, FILE* file, char* c, int* row, int* col, bool* found){
 	node* nNode = newNode(nonterms[17]);
 	if(tk->ID == IFTK){
@@ -205,6 +222,7 @@ node* ifchk(Ttoken* tk, FILE* file, char* c, int* row, int* col, bool* found){
 	}
 	return NULL;
 }
+//required node for out pattern: cout <expression>
 node* out(Ttoken* tk, FILE* file, char* c, int* row, int* col, bool* found){
 	node* nNode = newNode(nonterms[16]);
 	if(tk->ID == COUTTK){
@@ -216,6 +234,7 @@ node* out(Ttoken* tk, FILE* file, char* c, int* row, int* col, bool* found){
 	}
 	return NULL;
 }
+//required node for in pattern: cin Identidier
 node* in(Ttoken* tk, FILE* file, char* c, int* row, int* col, bool* found){
 	node* nNode = newNode(nonterms[15]);
 	if(tk->ID == CINTK){
@@ -232,6 +251,7 @@ node* in(Ttoken* tk, FILE* file, char* c, int* row, int* col, bool* found){
 	}
 	return NULL;
 }
+//required node for stat pattern: <in> ; | <out> ; | <block | <if> ; | <loop1> ; | <loop2> ; | <assign> ; | <goto> ; } <label> ; | <pick> ; 
 node* stat(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	node* nNode = newNode(nonterms[14]);
 	bool* found = malloc(sizeof(bool));
@@ -274,6 +294,7 @@ node* stat(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	nNode->one = block(tk, file, c, row, col);
 	return nNode;
 }
+//required node for mStat pattern: [empty] | <stat> <mStat>
 node* mStat(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	node* nNode = newNode(nonterms[13]);
 	if(tk->ID == CINTK || tk->ID == COUTTK ||tk->ID == OPENCURLTK ||tk->ID == IFTK ||tk->ID == WHILETK ||tk->ID == REPEATTK ||tk->ID == SETTK ||tk->ID == JUMPTK ||tk->ID == LABELTK ||tk->ID == PICKTK){		
@@ -282,12 +303,14 @@ node* mStat(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	}
 	return nNode;
 }
+//required node for stats pattern: <stat> <mStat>
 node* stats(Ttoken* tk, FILE* file, char* c, int* row, int* col){	
 	node* nNode = newNode(nonterms[12]);
 	nNode->one = stat(tk, file, c, row, col);
 	nNode->two = mStat(tk, file, c, row, col);
 	return nNode;
 }
+//required node for R pattern: ( <expression> ) | IDTK | INTTK
 node* R(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	node* nNode = newNode(nonterms[11]);
 	if(tk->ID == OPENPARENTK){
@@ -315,6 +338,7 @@ node* R(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	error();
 	return NULL;
 }
+//required node for M pattern: ^ <M> | <R>
 node* M(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	node* nNode = newNode(nonterms[10]);
 	if(tk->ID == EXPONTK){
@@ -326,7 +350,7 @@ node* M(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	nNode->one = R(tk, file, c, row, col);
 	return nNode;
 }
-
+//required node for A pattern: <M> * <A> | <M>
 node* A(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	node* nNode = newNode(nonterms[9]);
 	nNode->one = M(tk, file, c, row, col);
@@ -337,6 +361,7 @@ node* A(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	}
 	return nNode;
 }
+//additional left recursion-removing node for N1 pattern: / <A> <N1> | - <A> <N1> | [empty]
 node* N1(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	node* nNode = newNode(nonterms[8]);
 	if(tk->ID == DIVIDETK){
@@ -346,7 +371,7 @@ node* N1(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 		nNode->three = N1(tk, file, c, row, col);
 		return nNode;
 	}
-	if(tk->ID == MINUSTK){
+	if(tk->ID == PLUSTK){
 		nNode->one = newTermNode(tk);
 		*tk = scanner(file, c, row, col);
 		nNode->two = A(tk, file, c, row, col);
@@ -355,12 +380,14 @@ node* N1(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	}
 	return nNode;
 }
+//edited for left recursion required node for N pattern: <A> <N1> 
 node* N(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	node* nNode = newNode(nonterms[7]);
 	nNode->one = A(tk, file, c, row, col);
 	nNode->two = N1(tk, file, c, row, col);
 	return nNode;
 }
+//required node for expression pattern: <N> - <expression> | <N>
 node* expr(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	node* nNode = newNode(nonterms[6]);
 	nNode-> one = N(tk, file, c, row, col);
@@ -371,7 +398,7 @@ node* expr(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	}
 	return nNode;
 }
-//factorvars is complete
+//additional node for shrinking to 4 branch nodes for facvars pattern: create IDTK := | create IDTk;
 node* facvars(Ttoken* tk, FILE* file, char* c, int* row, int* col, bool* coleql){
 	node* nNode = newNode(nonterms[5]);
 	if(tk->ID == CREATETK){
@@ -396,10 +423,10 @@ node* facvars(Ttoken* tk, FILE* file, char* c, int* row, int* col, bool* coleql)
 	error();
 	return NULL;
 }
-//vars is complete
+//edited required node for vars pattern: [empty] | <facvars> | <facvars> INTTK ; <vars>
 node* vars(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	node* nNode = newNode(nonterms[4]);
-	bool* coleql = malloc(sizeof(bool));
+	bool* coleql = malloc(sizeof(bool));//awkward implementation to shrink size
 	*coleql = false;
 	if(tk->ID == CREATETK){
 		nNode->one = facvars(tk, file, c, row, col, coleql);
@@ -421,7 +448,7 @@ node* vars(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	}
 	return nNode;
 }
-
+//required node for block pattern: { <vars> <stats }
 node* block(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	node* nNode = newNode(nonterms[3]);
 	if(tk->ID != OPENCURLTK){
@@ -440,6 +467,7 @@ node* block(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	*tk = scanner(file, c, row, col);
 	return nNode;
 }
+//required node for func pattern: func IDTK <block>
 node* func(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	node* nNode = newNode(nonterms[2]);
 	if(tk->ID == FUNCTK){	
@@ -455,7 +483,7 @@ node* func(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	error();
 	return NULL;
 }
-//Identifies BNF for program
+//required node for program pattern: <vars> tape <func> <block | <vars> tape <block>
 node* program(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	node* root = newNode(nonterms[1]);
 	root->one = vars(tk,file,c,row,col);
@@ -474,7 +502,7 @@ node* program(Ttoken* tk, FILE* file, char* c, int* row, int* col){
 	error();
 	return NULL;
 }
-
+//calls the parser to generate the tree
 void parser(FILE* file){
 	//create malloc'd variables to send to other functions
 	int *col = malloc(sizeof(int));
